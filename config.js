7cd804089fe926969ed3619e6dd3f6e58358b019
LauraWaldron <lwaldron2021@fau.edu>
2023-06-07 14:05:01 -0400

    Added new files

---
+++ config.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBqGVjBtVjkMUbPt0yEOYMLFJMIjgCiess",
+  authDomain: "geeksquad-cd084.firebaseapp.com",
+  projectId: "geeksquad-cd084",
+  storageBucket: "geeksquad-cd084.appspot.com",
+  messagingSenderId: "838536645169",
+  appId: "1:838536645169:web:29f9b273cd647ecaf9a310",
+  measurementId: "G-6J0P0FERT7"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth=firebase.auth();
+const db=firebase.firestone();
