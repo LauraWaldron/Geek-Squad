7cce0b02d22343656a988ff20dd77c4332e45bed
phamj0 <107446014+phamj0@users.noreply.github.com>
2023-07-10 13:02:53 -0400

    removed unnessary code for grid

--- scripts.js
+++ scripts.js
@@ -7,83 +7,19 @@
  */
 $(function () {
 
-    /*Button that when clicked, starts startNewGame*/
-    $('#clear').append(
-        $(document.createElement('button')).prop({
-            type: 'button',
-            innerHTML: 'Clear', /*name inside html*/
-        }).click(function() {
-            startNewGame(); /*Calls to this function*/
-        })
-    );
-
     var squares = [],
-        SIZE = 9,
-        EMPTY = "&nbsp;",
-        score,
-        moves,
-        turn = "X",
-
-    /*
-     * To determine a win condition, each square is "tagged" from left
-     * to right, top to bottom, with successive powers of 2.  Each cell
-     * thus represents an individual bit in a 9-bit string, and a
-     * player's squares at any given time can be represented as a
-     * unique 9-bit value. A winner can thus be easily determined by
-     * checking whether the player's current 9 bits have covered any
-     * of the eight "three-in-a-row" combinations.
-     *
-     *     273                 84
-     *        \               /
-     *          1 |   2 |   4  = 7
-     *       -----+-----+-----
-     *          8 |  16 |  32  = 56
-     *       -----+-----+-----
-     *         64 | 128 | 256  = 448
-     *       =================
-     *         73   146   292
-     *
-     */
-    wins = [7, 56, 448, 73, 146, 292, 273, 84],
-
-    /*
-     * Clears the score and move count, erases the board, and makes it
-     * X's turn.
-     */
+        SIZE = 9
+      
     startNewGame = function () {
-        turn = "X";
-        score = {"X": 0, "O": 0};
-        moves = 0;
-        squares.forEach(function (square) {square.html(EMPTY);});
+        
     },
 
-    /*
-     * Returns whether the given score is a winning score.
-     */
-    win = function (score) {
-        for (var i = 0; i < wins.length; i += 1) {
-            if ((wins[i] & score) === wins[i]) {
-                return true;
-            }
-        }
-        return false;
-    },
 
-    /*
-     * Sets the clicked-on square to the current player's mark,
-     * then checks for a win or cats game.  Also changes the
-     * current player.
-     */
     set = function () {
 
         if ($(this).html() !== EMPTY) {
             return;
         }
-        $(this).html(turn);
-        console.log($(this));
-        moves += 1;
-        score[turn] += $(this)[0].indicator;
-        console.log(score[turn]);
         if (win(score[turn])) {
             alert(turn + " wins!");
             startNewGame();
@@ -95,11 +31,7 @@ $(function () {
         }
     },
 
-    /*
-     * Creates and attaches the DOM elements for the board as an
-     * HTML table, assigns the indicators for each cell, and starts
-     * a new game.
-     */
+
     play = function () {
         var board = $("<table border=1 cellspacing=0>"), indicator = 1;
         for (var i = 0; i < SIZE; i += 1) {
@@ -107,11 +39,7 @@ $(function () {
             board.append(row);
             for (var j = 0; j < SIZE; j += 1) {
                 var cell = $("<td height=50 width=50 align=center valign=center></td>");
-                cell[0].indicator = indicator;
-                cell.click(set);
                 row.append(cell);
-                squares.push(cell);
-                indicator += indicator;
             }
         }
 
