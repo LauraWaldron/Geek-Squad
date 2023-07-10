9b228298de5b36882dd1c5ffc2f7fa22e8870034
phamj0 <107446014+phamj0@users.noreply.github.com>
2023-07-10 15:20:22 -0400

    added function for grid size for each level

--- scripts.js
+++ scripts.js
@@ -1,21 +1,32 @@
-
-$(function () {
-    SIZE = 9
-    draw = function () {
-        var board = $("<table border=1 cellspacing=0>");
-        for (var i = 1; i <= SIZE; i += 1) {
-            var row = $("<tr>");
-            board.append(row);
-            for (var j = 1; j <= SIZE; j += 1) {
-                var cell = $("<td height=50 width=50 align=center valign=center></td>");
-                row.append(cell);
-            }
-        }
-
-        // Attach under tictactoe if present, otherwise to body.
-        $(document.getElementById("maze") || document.body).append(board);
-        startNewGame();
+$(function() {
+    var levelDifficulty = function() {
+        var path = window.location.pathname;
+      if (path == '/game1.html') {
+        return 6;
+      } else if ((path) == '/game2.html') {
+        return 9;
+      } else {
+        return 12;
+      }
     };
-
-    draw();
-});
+  
+    $(function() {
+      const GRID_SIZE = levelDifficulty();
+  
+      const draw = function() {
+        const grid = $("<table border='1' cellspacing='0'>");
+        for (let i = 1; i <= GRID_SIZE; i += 1) {
+          const row = $("<tr>");
+          grid.append(row);
+          for (let j = 1; j <= GRID_SIZE; j += 1) {
+            const cell = $("<td height='50' width='50' align='center' valign='center'></td>");
+            row.append(cell);
+          }
+        }
+        $(document.body).append(grid);
+      };
+  
+      draw();
+    });
+  });
+  
